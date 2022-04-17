34ae116779d0cf84eef3bc085ec37a993f96ee7f
Casey <caseyjtite@gmail.com>
2022-04-16 17:16:55 -0700

    begin building server/ db

---
+++ server/server.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+
+const PORT = 3009; // Client will be 3000
+const express = require("express");
+
+// middleware
+const morgan = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieSession = require("cookie-session");
+
+// database
+const { Pool } = require("pg");
+// const dbParams = require("./lib/db.js");
+// const db = new Pool(dbParams);
+// db.connect();
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["chicken", "horse", "cat"],
+  })
+);
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}!`);
+});
