d2ac659676522634744e607052b9f74568e95245
Casey <caseyjtite@gmail.com>
2022-04-18 11:31:14 -0700

    modify landing page

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,9 +1,21 @@
 import Header from "./Header";
+import AddLocationAltIcon from "@mui/icons-material/AddLocationAlt";
+import "../styles/Landing.css";
 const Landing = () => {
   return (
     <>
       <Header />
-      <h1>Welcome!</h1>
+      <div className="welcome">
+        <h1>
+          Welcome to <u>Maps!</u>
+        </h1>
+        <AddLocationAltIcon
+          sx={{
+            fontSize: "75px",
+            color: "hsl(83deg 67% 60%)",
+          }}
+        />
+      </div>
     </>
   );
 };
