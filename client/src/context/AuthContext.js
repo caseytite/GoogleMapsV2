2c20ceed6d43d908e3264b2b9287cd839f0ce56c
Casey <caseyjtite@gmail.com>
2022-10-10 14:17:37 -0700

    properly implement context

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useState, createContext } from "react";
+import mapStyles from "../mapStyles/mapStyles";
 
-const LoggedInUser = React.createContext({
+export const LoggedInUser = createContext({
   userID: localStorage.getItem("userId"),
+  style: mapStyles[0],
+  loggedIn: false,
+  changeStyle: () => {},
+  login: () => {},
+  logout: () => {},
 });
 
-export default LoggedInUser;
+const LoggedInUserProvider = (props) => {
+  const [loggedIn, setIsLoggedIn] = useState(
+    localStorage.getItem("userId") || false
+  );
+  const [style, setStyle] = useState(mapStyles[0]);
+
+  const changeStyleHandler = (styles) => {
+    const removeStyle = styles.shift();
+    styles.push(removeStyle);
+    setStyle(styles[0]);
+  };
+
+  const logInHandler = (userId) => {
+    localStorage.setItem("userId", userId);
+    setIsLoggedIn(true);
+  };
+
+  const logOutHandler = () => {
+    localStorage.clear();
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <LoggedInUser.Provider
+      value={{
+        login: logInHandler,
+        logout: logOutHandler,
+        loggedIn,
+        changeStyle: changeStyleHandler,
+        style,
+      }}
+    >
+      {props.children}
+    </LoggedInUser.Provider>
+  );
+};
+
+export default LoggedInUserProvider;
